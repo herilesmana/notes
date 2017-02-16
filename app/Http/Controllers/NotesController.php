1bee238a0596f9fbf325f6cad97a1a667f915fb3
zeniuscode <zeniuscode@gmail.com>
2017-02-17 05:19:59 +0700

    Method untuk tampil data

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Note;
 
 class NotesController extends Controller
 {
+	public function getNotes()
+	{
+		return Note::paginate(5);
+
+		
+	}
     public function index()
     {
         return view('layouts.master');
